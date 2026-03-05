5676a1b89e424a4f15c31f4fc42710aa70389695
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2026-03-05 03:30:06 +0100

    Add review-core package with Review entity
    Introduce a new @techweb-project/review-core package (ES module) with dependencies on core, auth-core, and restaurant-core. Add Review entity (id, content, rating, author: User, restaurant: Restaurant) and export it from src/index.js. Create a placeholder interfaces/review_repository.js and include a TODO note in the index for documentation follow-up.

---
+++ packages/backend/packages/review-core/src/entities/review.js
@@ -0,0 +1,34 @@
+import { User } from "@techweb-project/auth-core";
+import { Restaurant } from "@techweb-project/restaurant-core";
+
+export default class Review {
+    /** @type {number} */
+    id;
+
+    /** @type {string} */
+    content;
+
+    /** @type {number} */
+    rating;
+
+    /** @type {User} */
+    author;
+
+    /** @type {Restaurant} */
+    restaurant;
+
+    /**
+     * @param {number} id
+     * @param {string} content
+     * @param {number} rating
+     * @param {User} author
+     * @param {Restaurant} restaurant
+     */
+    constructor(id, content, rating, author, restaurant) {
+        this.id = id;
+        this.content = content;
+        this.rating = rating;
+        this.author = author;
+        this.restaurant = restaurant;
+    }
+}
---
+++ packages/backend/packages/review-core/src/index.js
@@ -0,0 +1,4 @@
+import Review from "./entities/review.js";
+export { Review };
+
+/// TODO: Leggi la documentazione scritta sul foglio per favore
