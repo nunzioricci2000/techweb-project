0b7d5a109f6e3777017bab375e077342c749169c
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2026-03-12 22:06:03 +0100

    Use core LoggerService and add info method
    Switch the LoggerService import to the new core package (@techweb-project/core) and add an info(...args) method that delegates to console.info. Existing log and error methods are unchanged.

--- packages/backend/packages/console-logger/src/index.js
+++ packages/backend/packages/console-logger/src/index.js
@@ -1,8 +1,12 @@
-import LoggerService from '@techweb-backend/logger-service';
+import { LoggerService } from '@techweb-project/core';
 
 export default class ConsoleLogger extends LoggerService {
     log(...args) {
         console.log(...args);
+    }
+
+    info(...args) {
+        console.info(...args);
     }
 
     error(...args) {
